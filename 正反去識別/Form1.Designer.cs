4536b6aeb8d703a28bfde64b8cc2d9f7b791e9d1
JASON <jason6956@gmail.com>
2019-01-09 11:04:35 +0800

    init

---
+++ 正反去識別/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace 正反去識別
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDeFZ = new System.Windows.Forms.TextBox();
+            this.txtFZ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFZ = new System.Windows.Forms.Button();
+            this.btnDeFZ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDeFZ
+            // 
+            this.txtDeFZ.Location = new System.Drawing.Point(80, 30);
+            this.txtDeFZ.Name = "txtDeFZ";
+            this.txtDeFZ.Size = new System.Drawing.Size(100, 22);
+            this.txtDeFZ.TabIndex = 0;
+            // 
+            // txtFZ
+            // 
+            this.txtFZ.Location = new System.Drawing.Point(80, 74);
+            this.txtFZ.Name = "txtFZ";
+            this.txtFZ.Size = new System.Drawing.Size(100, 22);
+            this.txtFZ.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "未去識別";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "去識別";
+            // 
+            // btnFZ
+            // 
+            this.btnFZ.Location = new System.Drawing.Point(12, 122);
+            this.btnFZ.Name = "btnFZ";
+            this.btnFZ.Size = new System.Drawing.Size(75, 23);
+            this.btnFZ.TabIndex = 4;
+            this.btnFZ.Text = "去識別";
+            this.btnFZ.UseVisualStyleBackColor = true;
+            this.btnFZ.Click += new System.EventHandler(this.btnFZ_Click);
+            // 
+            // btnDeFZ
+            // 
+            this.btnDeFZ.Location = new System.Drawing.Point(105, 122);
+            this.btnDeFZ.Name = "btnDeFZ";
+            this.btnDeFZ.Size = new System.Drawing.Size(75, 23);
+            this.btnDeFZ.TabIndex = 5;
+            this.btnDeFZ.Text = "反去識別";
+            this.btnDeFZ.UseVisualStyleBackColor = true;
+            this.btnDeFZ.Click += new System.EventHandler(this.btnDeFZ_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 166);
+            this.Controls.Add(this.btnDeFZ);
+            this.Controls.Add(this.btnFZ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFZ);
+            this.Controls.Add(this.txtDeFZ);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "正反去識別化";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDeFZ;
+        private System.Windows.Forms.TextBox txtFZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFZ;
+        private System.Windows.Forms.Button btnDeFZ;
+    }
+}
+
---
+++ 正反去識別/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 正反去識別
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 去識別化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFZ_Click(object sender, EventArgs e)
+        {
+            String rtn = ""; //回傳         
+            
+            int num = txtDeFZ.Text.Length; //資料長度
+            string[] txt = new string[num]; //轉字串
+
+            int i = 0;
+            foreach (char s in txtDeFZ.Text)
+            {
+                txt[i] = s.ToString();
+                i += 1;
+            }
+
+            //14碼帳號：前6碼不轉
+            //10碼身份證：前2碼不轉
+            if (num == 10 || num == 11)
+            { //ID
+                #region 檢查ID
+                string result = IDChk(txtDeFZ.Text.Substring(0, 10));
+
+                if (result != "0")
+                {
+                    if (result == "1") { MessageBox.Show("字數不到10"); return; }
+                    if (result == "2") { MessageBox.Show("第二碼非1,2"); return; }
+                    if (result == "3") { MessageBox.Show("首碼有誤"); return; }
+                    if (result == "4") { MessageBox.Show("檢查碼不對"); return; }
+                }                
+                #endregion 
+
+                txt[2] = fz(txt[2].ToString());
+                txt[3] = fz(txt[3].ToString());
+                txt[4] = fz(txt[4].ToString());
+                txt[5] = fz(txt[5].ToString());
+                txt[6] = fz(txt[6].ToString());
+                txt[7] = fz(txt[7].ToString());
+                txt[8] = fz(txt[8].ToString());
+                txt[9] = fz(txt[9].ToString());
+
+                foreach (string s in txt)
+                {
+                    rtn += s.ToString();                    
+                }
+            }
+            else if (num == 14)
+            { //ACT
+                txt[6] = fz(txt[6].ToString());
+                txt[7] = fz(txt[7].ToString());
+                txt[8] = fz(txt[8].ToString());
+                txt[9] = fz(txt[9].ToString());
+                txt[10] = fz(txt[10].ToString());
+                txt[11] = fz(txt[11].ToString());
+                txt[12] = fz(txt[12].ToString());
+                txt[13] = fz(txt[13].ToString());
+                txt[14] = fz(txt[14].ToString());
+
+                foreach (string s in txt)
+                {
+                    rtn += s.ToString();
+                }
+            }
+            else {
+                MessageBox.Show("去識別化有誤");
+            }
+
+            txtFZ.Text = rtn;
+        }
+
+        /// <summary>
+        /// 反去識別
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeFZ_Click(object sender, EventArgs e)
+        {
+            String rtn = ""; //回傳         
+
+            int num = txtFZ.Text.Length; //資料長度
+            string[] txt = new string[num]; //轉字串
+
+            int i = 0;
+            foreach (char s in txtFZ.Text)
+            {
+                txt[i] = s.ToString();
+                i += 1;
+            }
+
+            //14碼帳號：前6碼不轉
+            //10碼身份證：前2碼不轉
+            if (num == 10 || num == 11)
+            { //ID
+                txt[2] = defz(txt[2].ToString());
+                txt[3] = defz(txt[3].ToString());
+                txt[4] = defz(txt[4].ToString());
+                txt[5] = defz(txt[5].ToString());
+                txt[6] = defz(txt[6].ToString());
+                txt[7] = defz(txt[7].ToString());
+                txt[8] = defz(txt[8].ToString());
+                txt[9] = defz(txt[9].ToString());
+
+                foreach (string s in txt)
+                {
+                    rtn += s.ToString();
+                }
+            }
+            else if (num == 14)
+            { //ACT
+                txt[6] = defz(txt[6].ToString());
+                txt[7] = defz(txt[7].ToString());
+                txt[8] = defz(txt[8].ToString());
+                txt[9] = defz(txt[9].ToString());
+                txt[10] = defz(txt[10].ToString());
+                txt[11] = defz(txt[11].ToString());
+                txt[12] = defz(txt[12].ToString());
+                txt[13] = defz(txt[13].ToString());
+                txt[14] = defz(txt[14].ToString());
+
+                foreach (string s in txt)
+                {
+                    rtn += s.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("反去識別化有誤");
+            }
+
+            #region 檢查ID
+            string result = IDChk(rtn.Substring(0, 10));
+
+            if (result != "0")
+            {
+                if (result == "1") { MessageBox.Show("字數不到10"); return; }
+                if (result == "2") { MessageBox.Show("第二碼非1,2"); return; }
+                if (result == "3") { MessageBox.Show("首碼有誤"); return; }
+                if (result == "4") { MessageBox.Show("檢查碼不對"); return; }
+            }
+            #endregion 
+
+            txtDeFZ.Text = rtn;
+        }
+
+        //未去識別->去識別
+        //1->9
+        //2->7
+        //3->5
+        //4->3
+        //5->1
+        //6->0
+        //7->8
+        //8->6
+        //9->4
+        //0->2       
+        public string fz(string s)
+        {
+            string t;
+            switch (s)
+            {
+                case "0":
+                    t = "2";
+                    break;
+                case "1":
+                    t = "9";
+                    break;
+                case "2":
+                    t = "7";
+                    break;
+                case "3":
+                    t = "5";
+                    break;
+                case "4":
+                    t = "3";
+                    break;
+                case "5":
+                    t = "1";
+                    break;
+                case "6":
+                    t = "0";
+                    break;
+                case "7":
+                    t = "8";
+                    break;
+                case "8":
+                    t = "6";
+                    break;
+                case "9":
+                    t = "4";
+                    break;
+                default:
+                    t="";
+                    break;
+            }
+
+            return t;        
+        }
+
+        public string defz(string s)
+        {
+            string t;
+            switch (s)
+            {
+                case "2":
+                    t = "0";
+                    break;
+                case "9":
+                    t = "1";
+                    break;
+                case "7":
+                    t = "2";
+                    break;
+                case "5":
+                    t = "3";
+                    break;
+                case "3":
+                    t = "4";
+                    break;
+                case "1":
+                    t = "5";
+                    break;
+                case "0":
+                    t = "6";
+                    break;
+                case "8":
+                    t = "7";
+                    break;
+                case "6":
+                    t = "8";
+                    break;
+                case "4":
+                    t = "9";
+                    break;
+                default:
+                    t = "";
+                    break;
+            }
+
+            return t;
+        }
+
+        /// <summary>
+        /// 身份証檢核
+        /// </summary>
+        /// <param name="vid"></param>
+        /// <returns>
+        /// 回傳1:字數不到10  
+        /// 回傳2:第二碼非1,2  
+        /// 回傳3:首碼有誤  
+        /// 回傳4:檢查碼不對  
+        /// </returns>
+        public string IDChk(string vid)
+        {
+            List<string> FirstEng = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "W", "Z", "I", "O" };
+            string aa = vid.ToUpper();
+            bool chackFirstEnd = false;
+            if (aa.Trim().Length == 10)
+            {
+                byte firstNo = Convert.ToByte(aa.Trim().Substring(1, 1));
+                if (firstNo > 2 || firstNo < 1)
+                {
+                    return "2";
+                }
+                else
+                {
+                    int x;
+                    for (x = 0; x < FirstEng.Count; x++)
+                    {
+                        if (aa.Substring(0, 1) == FirstEng[x])
+                        {
+                            aa = string.Format("{0}{1}", x + 10, aa.Substring(1, 9));
+                            chackFirstEnd = true;
+                            break;
+                        }
+                    }
+                    if (!chackFirstEnd)
+                        return "3";
+
+                    int i = 1;
+                    int ss = int.Parse(aa.Substring(0, 1));
+                    while (aa.Length > i)
+                    {
+                        ss = ss + (int.Parse(aa.Substring(i, 1)) * (10 - i));
+                        i++;
+                    }
+                    aa = ss.ToString();
+                    if (vid.Substring(9, 1) == "0")
+                    {
+                        if (aa.Substring(aa.Length - 1, 1) == "0")
+                        {
+                            return "0";
+                        }
+                        else
+                        {
+                            return "4";
+                        }
+                    }
+                    else
+                    {
+                        if (vid.Substring(9, 1) == (10 - int.Parse(aa.Substring(aa.Length - 1, 1))).ToString())
+                        {
+
+                            return "0";
+                        }
+                        else
+                        {
+                            return "4";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return "1";
+            }
+        } 
+    }
+}
